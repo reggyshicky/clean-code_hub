471a4c03fc6dc3233b904c1d8b9f92d6327ea244
reggyshicky <reginahshikanda@gmail.com>
2023-05-10 23:11:37 -0700

    how to create and use a module

---
+++ python/fibo.py
@@ -0,0 +1,16 @@
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')  # write fibonacci series upto n
+        a, b = b, a + b
+    print()
+
+
+def fib2(n):  # return fibonacci series upto n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+    return result
+# how to make a module, then on the terminal enter python3 to access python interpreter, then import fibo, (comes from fibo.py)... and then use fibo.fib(arg) to call your function, the same tofibo.fib2(arg). if you using a function more often, you ca always do this >>>fib = fibo.fib  and then >>>fib(arg)
