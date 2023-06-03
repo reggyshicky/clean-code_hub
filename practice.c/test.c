eb5a3c63dc0ec955b9707b825aa4d2628a5e35b2
reggyshicky <reginahshikanda@gmail.com>
2023-06-03 14:12:19 +0300

    unittest

---
+++ practice.c/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - trying to use double \n
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("Hello i love programming alot\n");
+	return (0);
+}
