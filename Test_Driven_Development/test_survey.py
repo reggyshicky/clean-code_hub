3a31bac4b3459d9376441e8b994f8f2f46b93e1f
reggyshicky <reginahshikanda@gmail.com>
2023-06-04 13:23:39 +0300

    added another test method in the test file to accept three responses from the user , not only one

--- Test_Driven_Development/test_survey.py
+++ Test_Driven_Development/test_survey.py
@@ -12,5 +12,17 @@ class TestAnonymousSurvey(unittest.TestCase):
 
         self.assertIn('English', my_survey.responses)
 
+    def test_store_three_responses(self):
+        """Test tha three individual responses are stored properly."""
+
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+
 
 unittest.main()
