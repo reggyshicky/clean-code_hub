c89d71c4f820a4dc63c62379d1f9d871e4034df1
reggyshicky <reginahshikanda@gmail.com>
2023-06-04 10:49:45 +0300

    i introduced a failing test by adding a third parameter on my function file

--- Test_Driven_Development/city_functions.py
+++ Test_Driven_Development/city_functions.py
@@ -1,4 +1,4 @@
-def get_city_details(city_name, country_name):
+def get_city_details(city_name, country_name, population):
     """Generate a neatly concatenated city details"""
-    city_details = city_name + " " + country_name
+    city_details = city_name + " " + country_name + " - population " +  population
     return city_details.title()
