fd1f27435d437ddfccdf69b649113e740c4323a2
reggyshicky <reginahshikanda@gmail.com>
2023-06-08 02:49:14 -0700

    testing whethet pycodestyle captures the paramenter naming convention, no single letter passed

--- python/fibo.py
+++ python/fibo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def fib(n):
     a, b = 0, 1
     while a < n:
@@ -13,4 +14,9 @@ def fib2(n):  # return fibonacci series upto n
         result.append(a)
         a, b = b, a + b
     return result
+
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
+
 # how to make a module, then on the terminal enter python3 to access python interpreter, then import fibo, (comes from fibo.py)... and then use fibo.fib(arg) to call your function, the same tofibo.fib2(arg). if you using a function more often, you ca always do this >>>fib = fibo.fib  and then >>>fib(arg)
