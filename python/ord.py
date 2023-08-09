5801f78797ed9638e45d8db8e983e96fe8b24135
reggyshicky <reginahshikanda@gmail.com>
2023-08-09 10:35:50 +0300

    use of ord() to return a integer of any unicode character

---
+++ python/ord.py
@@ -0,0 +1,3 @@
+#The ord() function in python will return an integer tat represents the unicode character passed
+#to it. It takes a single parameter, i.e the below code will return 65
+print(ord('A'))
