62bb413cebd44398f135f5e0328824119dcc1bce
reggyshicky <reginahshikanda@gmail.com>
2023-08-09 11:39:47 +0300

    try and except block

---
+++ python/try_except.py
@@ -0,0 +1,12 @@
+def division(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Error, denominator should not be zero")
+    finally:
+        print("Division completed")
+
+ans = division(4, 0)
+print(ans)
+ans21 = division(4,2)
+print(ans21)
