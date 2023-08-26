4e97ea4a11bc0851ffba5a7e7a7cae9d97b59f9a
reggyshicky <reginahshikanda@gmail.com>
2023-08-27 01:44:08 +0300

    anonymous/expression functions. Arrow functions, and declared functions

--- javascript/score.js
+++ javascript/score.js
@@ -1,11 +1,14 @@
 // Calculate the average score for a team
-function calculateAverage(scores) {
-  const total = scores.reduce((sum, score) => sum + score, 0);
+const calculateAverage = scores => {
+  let total = 0;
+  for (const score of scores) {
+    total += score;
+  }
   return total / scores.length;
-}
+};
 
 // Determine the winner or draw between two teams
-function determineWinnerOrDraw(team1, team2) {
+const checkWinner = function(team1, team2) {
   const averageScoreTeam1 = calculateAverage(team1);
   const averageScoreTeam2 = calculateAverage(team2);
 
@@ -24,15 +27,15 @@ function determineWinnerOrDraw(team1, team2) {
 //averageScoreTeam1 >= 100 && averageScoreTeam2 >= 100
 // ? averageScoreTeam1 > averageScoreTeam2
 //    : 
-const manuTestData1 = [96, 108, 89];
-const arsenalTestData1 = [88, 91, 110];
+const manuTestData1 = [44, 23, 71];
+const arsenalTestData1 = [ 85, 54, 41];
 
-const manuTestData2 = [97, 112, 101];
-const arsenalTestData2 = [109, 95, 123];
+const manuTestData2 = [85, 54, 41];
+const arsenalTestData2 = [23, 34, 27];
 
 // Determine the winner or draw for each test data set
 console.log("Test Data 1:");
-console.log("Winner:", determineWinnerOrDraw(manuTestData1, arsenalTestData1));
+console.log("Winner:", checkWinner(manuTestData1, arsenalTestData1));
 
 console.log("\nTest Data 2:");
-console.log("Winner:", determineWinnerOrDraw(manuTestData2, arsenalTestData2));
+console.log("Winner:", checkWinner(manuTestData2, arsenalTestData2));
